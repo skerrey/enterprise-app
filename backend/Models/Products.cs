9f73b057ce4683cf7dc0dd1c838999dbd04ea49e
Seth Kerrey <sethkerrey@gmail.com>
2025-07-24 11:13:30 -0500

    update model

--- backend/Models/Products.cs
+++ backend/Models/Products.cs
@@ -5,5 +5,7 @@
         public int id { get; set; }
         public string label { get; set; }
         public decimal price { get; set; }
+        public int quantity { get; set; } 
+        public decimal total { get; set; }
     }
 }

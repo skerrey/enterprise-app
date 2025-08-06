58bf7fed1e96d78379968f06dbecf7116b67dbd4
Seth Kerrey <sethkerrey@gmail.com>
2025-08-05 19:15:40 -0500

    add placeholder img

--- frontend/src/layout/AppSidebar.tsx
+++ frontend/src/layout/AppSidebar.tsx
@@ -68,7 +68,7 @@ const AppSidebar: React.FC = () => {
         <Link to="/">
           {isExpanded || isHovered || isMobileOpen ? (
             <>
-              <img
+              {/* <img
                 className="dark:hidden"
                 src="/images/logo/logo.svg"
                 alt="Logo"
@@ -81,7 +81,26 @@ const AppSidebar: React.FC = () => {
                 alt="Logo"
                 width={150}
                 height={40}
-              />
+              /> */}
+              {/* Logo and Brand Area */}
+              <div className="flex flex-col items-start space-y-3">
+                <div className="flex items-center space-x-3">
+                  <div className="flex-shrink-0">
+                    <svg width="32" height="32" viewBox="0 0 63 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-blue-600 dark:text-blue-400">
+                      <path d="M63 29.3506L38.117 5.08907C37.4008 4.39081 36.4402 4 35.44 4C31.9983 4 30.2991 8.18294 32.7659 10.583L59.4712 36.5666C60.7577 37.8183 59.8715 40 58.0765 40H9.65685C8.59599 40 7.57857 39.5786 6.82843 38.8284L1.17157 33.1716C0.421426 32.4214 0 31.404 0 30.3431V10.6484L24.883 34.9109C25.5992 35.6092 26.5598 36 27.5601 36C31.0017 36 32.7009 31.8171 30.2342 29.417L3.52882 3.43345C2.24227 2.18166 3.12849 0 4.92354 0L53.3431 0C54.404 0 55.4214 0.421427 56.1716 1.17157L61.8284 6.82843C62.5786 7.57857 63 8.59599 63 9.65685V29.3506Z" fill="currentColor"/>
+                    </svg>
+                  </div>
+                  <div className="flex flex-col">
+                    <h1 className="text-lg font-bold text-gray-900 dark:text-white leading-tight">
+                      Request Dashboard
+                    </h1>
+                    <p className="text-sm font-medium text-gray-600 dark:text-gray-300 leading-tight">
+                      Advisory Services
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full h-px bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 dark:from-gray-700 dark:via-gray-600 dark:to-gray-700"></div>
+              </div>
             </>
           ) : (
             <img
